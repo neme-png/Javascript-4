cb228927497df1c490c9ce25afef5ba6557e5e12
Fabio Santangelo <fabbomaui04gmail.com>
2024-11-18 10:32:06 +0100

    Completamento esercizio javascript 4

---
+++ main.js
@@ -0,0 +1,15 @@
+let firstNumber = Number(prompt("Inserisci il primo numero"));
+
+let secondNumber = Number(prompt("Inserisci il secondo numero"));
+
+let somma = (firstNumber + secondNumber);
+
+let differenza = (firstNumber - secondNumber);
+
+let moltiplicazione = (firstNumber * secondNumber);
+
+let divisione = (firstNumber / secondNumber);
+
+let potenza = (firstNumber ** secondNumber);
+
+console.log(somma, differenza, moltiplicazione, divisione, potenza);
